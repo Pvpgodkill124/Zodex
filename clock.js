7b47ebfe413f74bf89146cd878a5dd046902b097
Pvpgodkill124 <127505101+Pvpgodkill124@users.noreply.github.com>
2025-07-10 18:48:25 +0530

    Delete clock.js

--- clock.js
+++
@@ -1,9 +0,0 @@
-function updateClock() {
-  const clock = document.getElementById("clock");
-  const now = new Date();
-  const time = now.toLocaleTimeString();
-  clock.textContent = time;
-}
-
-setInterval(updateClock, 1000);
-updateClock();
