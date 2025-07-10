5ea7ad953b1ca90ffa3d1010925b59a9c03c7094
Pvpgodkill124 <127505101+Pvpgodkill124@users.noreply.github.com>
2025-07-10 17:39:08 +0530

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,9 @@
+function updateClock() {
+  const clock = document.getElementById("clock");
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+  clock.textContent = time;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
